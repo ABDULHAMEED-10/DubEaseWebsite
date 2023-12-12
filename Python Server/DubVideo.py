18d769f32ae64956f6db0b6b93dfc8afed998949
abdulhameed10 <abdulhameed000650@gmail.com>
2023-12-12 19:12:31 +0500

    remove working

---
+++ Python Server/DubVideo.py
@@ -0,0 +1,50 @@
+from moviepy.video.io.VideoFileClip import VideoFileClip
+import moviepy.editor as mp
+import speech_recognition as sr
+from googletrans import Translator
+import os
+
+def extract_audio(video_path, audio_output_path):
+    video_clip = VideoFileClip(video_path)
+    os.makedirs(os.path.dirname(audio_output_path), exist_ok=True)
+    audio_clip = video_clip.audio
+    audio_clip.write_audiofile(audio_output_path)
+    video_clip.close()
+
+def convert_audio_to_text(audio_path):
+    recognizer = sr.Recognizer()
+    with sr.AudioFile(audio_path) as source:
+        try:
+            audio_data = recognizer.record(source)
+            audio_text = recognizer.recognize_google(audio_data)
+        except sr.UnknownValueError:
+            raise Exception("Audio is inaudible. Please choose a different video.")
+        except sr.RequestError as e:
+            raise Exception(f"Could not request results from Google Speech Recognition service; {e}")
+    return audio_text
+
+def translate_text(text, target_language='urdu'):
+    translator = Translator()
+    translation = translator.translate(text, dest=target_language)
+    return translation.text
+
+def save_text_to_file(text, output_file):
+    with open(output_file, 'w', encoding='utf-8') as file:
+        file.write(text)
+
+def process_video(filename):
+    video_path = os.path.join('E:/DubEase/Python Server/uploads/', filename)
+    audio_output_path = 'output/myaudio.wav'
+    text_output_path = 'output/translated_text.txt'
+
+    # Extract audio from the video
+    extract_audio(video_path, audio_output_path)
+
+    # Convert audio to text
+    audio_text = convert_audio_to_text(audio_output_path)
+
+    # Translate the text
+    translated_text = translate_text(audio_text)
+
+    # Save translated text to a file
+    save_text_to_file(translated_text, text_output_path)
--- frontend/src/Home/pages/StartVideoDubbing.js
+++ frontend/src/Home/pages/StartVideoDubbing.js
@@ -1,4 +1,4 @@
-import React, {Fragment,useState, useCallback, useRef, useEffect } from "react";
+import React, { Fragment, useState, useCallback, useRef, useEffect } from "react";
 import "../CSS/StartDubbing.css";
 import Webcam from "react-webcam";
 import MetaData from "../../layout/MetaData";
@@ -12,14 +12,20 @@ import Loader from "../../layout/Loader/Loader";
 import { generate_Dub } from "../../actions/dubbingAction";
 
 
+
 const StartVideoRecordUpload = () => {
   const [selectedVideo, setSelectedVideo] = useState(null);
   const [videoFileName, setVideoFileName] = useState(null);
+
   const [source, setSource] = useState(null);
+  const [videoUploaded, setVideoUploaded] = useState(false);
+  const [audioUploaded, setAudioUploaded] = useState(false);
 
-
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [audioFileName, setAudioFileName] = useState(null);
+  const VideoTab = useRef(null);
+  const switcherTab = useRef(null);
   const dispatch = useDispatch();
-
   const alert = useAlert();
 
 
@@ -27,22 +33,23 @@ const StartVideoRecordUpload = () => {
     setVideoFileName(e.target.files[0]);
   };
 
-  const VideoTab = useRef(null);
-  const switcherTab = useRef(null);
 
   const handleVideoChange = (event) => {
     const file = event.target.files[0];
-    
-    
-    if (file) {
 
-      const validVideoTypes = ['video/mp4','video/webm']; 
+
+    if (file) {
+      const validVideoTypes = ['video/mp4', 'video/webm'];
       if (validVideoTypes.includes(file.type)) {
+
+        setVideoFileName(null);
         setSource(file)
+        setVideoUploaded(true);
         setSelectedVideo(URL.createObjectURL(file));
         onFileChangeVideo(event);
       } else {
         setSelectedVideo(null);
+        setVideoUploaded(false);
         alert.error('Please select a valid video file');
       }
     } else {
@@ -50,7 +57,7 @@ const StartVideoRecordUpload = () => {
       alert.error('Please select a video file');
     }
   };
-  
+
 
   /////////////////////////
   const webcamRef = useRef(null);
@@ -120,22 +127,21 @@ const StartVideoRecordUpload = () => {
   };
   /////////////////////////////////////////////
 
-  const [selectedFile, setSelectedFile] = useState(null);
-  const [audioFileName, setAudioFileName] = useState(null);
-
   const onFileChangeAudio = (e) => {
     setAudioFileName(e.target.files[0]);
   };
   const handleAudioChange = (event) => {
     const file = event.target.files[0];
     if (file) {
-      const validAudioTypes = ['audio/mpeg', 'audio/wav', 'audio/mp3']; 
+      const validAudioTypes = ['audio/mpeg', 'audio/wav', 'audio/mp3'];
       if (validAudioTypes.includes(file.type)) {
         setSource(file);
+        setAudioUploaded(true);
         setSelectedFile(URL.createObjectURL(file));
         onFileChangeAudio(event);
       } else {
         setSelectedFile(null);
+        setAudioUploaded(false);
         alert.error('Please select a valid audio file');
       }
     } else {
@@ -143,7 +149,7 @@ const StartVideoRecordUpload = () => {
       alert.error('Please select an audio file');
     }
   };
-  
+
   const addAudioElement = (blob) => {
     const url = URL.createObjectURL(blob);
     const audio = document.createElement("audio");
@@ -154,17 +160,27 @@ const StartVideoRecordUpload = () => {
 
   const StartDubbing = (e) => {
     e.preventDefault();
+    if (audioUploaded && videoUploaded) {
+      alert.error('Please upload either audio or video, not both.');
+      return;
+    }
     const myForm = new FormData();
-    myForm.append("source", source);
-    dispatch(generate_Dub(myForm));
-    
+    if (videoUploaded) {
+      myForm.append("source", source);
+      dispatch(generate_Dub(myForm));
+    } else if (audioUploaded) {
+      myForm.append("source", source);
+      dispatch(generate_Dub(myForm));
+    } else {
+      alert.error('Please upload either audio or video.');
+    }
   };
-  let {err,loading} = useSelector(
+  let { err, loading } = useSelector(
     (state) => state
   );
 
   useEffect(() => {
-    
+
     if (camera) {
       const timer = setTimeout(() => {
         setShowButtons(true);
@@ -173,191 +189,216 @@ const StartVideoRecordUpload = () => {
     } else {
       setShowButtons(false);
     }
-  }, [camera,err]);
+  }, [camera, err]);
+
+  const remove = () => {
+    if (videoUploaded) {
+      setSelectedVideo(null);
+      setVideoFileName(null);
+      setSource(null); 
+      setVideoUploaded(false);
+    
+    }
+    if(audioUploaded){
+      setSelectedFile(null);
+      setAudioFileName(null);
+      setSource(null); 
+      setAudioUploaded(false);
+    }
+  }
+
   return (
     <Fragment>
-    {loading ? (
-      <Loader />
-    ) : (
-          <Fragment>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
           <MetaData title="Dub Video" />
-            
-    <div className="container-fluid bg-dark text-white container">
-      <div className="vh-100 toogleContainer col-md-8">
-        <div className="Header">
-          <div className="Audio_Video_toggle">
-            <p>Orignal Content</p>
-            <p>Dubbed Content</p>
-          </div>
-          <button ref={switcherTab} className="shifter"></button>
-        </div>
 
-        {/* -------------------------------------------------------- */}
-                {/* video part */}
-        
-        <div className="mainVideoBox" ref={VideoTab}>
-          {/* video box start  */}
-          <div className="videoBox ">
-            <div className="orignalVideoBox">
-              <ReactPlayer
-                controls
-                playIcon
-                url={selectedVideo}
-                width="100%"
-                height="100%"
-              />
-            </div>
+          <div className="container-fluid bg-dark text-white container">
+            <div className="vh-100 toogleContainer col-md-8">
+              <div className="Header">
+                <div className="Audio_Video_toggle">
+                  <p>Orignal Content</p>
+                  <p>Dubbed Content</p>
+                </div>
+                <button ref={switcherTab} className="shifter"></button>
+              </div>
 
-            <div className="dubbedVideoBox"></div>
+              {/* -------------------------------------------------------- */}
+              {/* video part */}
+
+              <div className="mainVideoBox" ref={VideoTab}>
+                {/* video box start  */}
+                <div className="videoBox ">
+                  <div className="orignalVideoBox">
+                    <ReactPlayer
+                      controls
+                      playIcon
+                      url={selectedVideo}
+                      width="100%"
+                      height="100%"
+                    />
+                  </div>
+
+                  <div className="dubbedVideoBox"></div>
+                </div>
+
+                {/* video box end */}
+                {/* audio box start */}
+                <div className="AudioBox ">
+                  <div className="orignalAudioBox">
+                      <ReactAudioPlayer
+                        src={selectedFile}
+                        controls
+                      />
+                    
+
+                  </div>
+
+                  <div className="dubbedAudioBox"></div>
+                </div>
+
+                <div className="mainUploadRecordVideoAudio" >
+                  <div className="uploadVideo">
+                    {!audioUploaded && <form className="browseVideo" id="myForm" method="POST" encType="multipart/form-data" onSubmit={StartDubbing}>
+                      <label htmlFor="inp">Browse Video</label>
+                      <label style={{ visibility: "hidden" }} htmlFor="inputButton"></label>
+
+                      {videoFileName?.name && (
+                        <p className="videoFileName">
+                          {videoFileName.name.split(" ").slice(0, 2).join(" ")}
+                          {videoFileName.name.split(" ").length > 2 && "..."}
+                        </p>
+
+                      )}
+                      <input
+                        type="file"
+                        id="inp"
+                        name="source"
+                        accept="video/mp4"
+                        style={{ display: "none" }}
+                        onChange={handleVideoChange}
+                      />
+
+                    </form>}
+                    
+
+
+                    {!videoUploaded && <form className="browseAudio" id="myForm" method="POST" encType="multipart/form-data" onSubmit={StartDubbing}>
+                      <label htmlFor="input-file">Browse Audio</label>
+                      <label style={{ visibility: "hidden" }} htmlFor="inputButton"></label>
+
+                      {audioFileName?.name && (
+                        <p className="audioFileName">
+                          {audioFileName.name.split(" ").slice(0, 2).join(" ")}
+                          {audioFileName.name.split(" ").length > 2 && "..."}
+                        </p>
+                      )}
+                      <input
+                        type="file"
+                        id="input-file"
+                        name="source"
+                        accept="audio/mp3"
+                        style={{ display: "none" }}
+                        onChange={handleAudioChange}
+                      />
+                      </form>}
+                    <Tooltip title="remove file">
+                    <button className="del_icon" onClick={remove}>
+                    <i className="fas fa-trash"></i>
+                        </button>
+                      </Tooltip>
+                  </div>
+
+                  <div className="recordAudio">
+
+                    <div className="recordVideo ">
+                      <Tooltip title="Start Recording">
+                        <button onClick={handleCameraClick}>
+                          {" "}
+                          <i className="fas fa-camera"></i>
+                        </button>
+                      </Tooltip>
+                      {recordedChunks.length > 0 && (
+                        <Tooltip title="Download Recording">
+                          <button onClick={handleDownload}>
+                            <i className="fas fa-download"></i>
+                          </button>
+                        </Tooltip>
+                      )}
                     </div>
-        
-          {/* video box end */}
-          {/* audio box start */}
-          <div className="AudioBox ">
-            <div className="orignalAudioBox">
-              <ReactAudioPlayer
-                src={selectedFile}
-                autoPlay
-                controls
-                
-              />
-              
-            </div>
-
-            <div className="dubbedAudioBox"></div>
-          </div>
-
-          <div className="mainUploadRecordVideoAudio" >
-            <div className="uploadVideo">
-              <form className="browseVideo" id="myForm" method="POST" encType="multipart/form-data" onSubmit={StartDubbing}>
-                <label htmlFor="inp">Browse Video</label>
-                <label style={{visibility:"hidden"}} htmlFor="inputButton"></label>
-                        
-                {videoFileName?.name && (
-                  <p className="videoFileName">
-                    {videoFileName.name.split(" ").slice(0, 2).join(" ")}
-                    {videoFileName.name.split(" ").length > 2 && "..."}
-                  </p>
-                )}
-                        <input
-                          type="file"
-                          id="inp"
-                          name="source"
-                          accept="video/mp4"
-                          style={{ display: "none" }}
-                          onChange={handleVideoChange} 
+                    <div className="recordAudioButton">
+                      <AudioRecorder
+                        onRecordingComplete={addAudioElement}
+                        audioTrackConstraints={{
+                          noiseSuppression: true,
+                          echoCancellation: true,
+                        }}
+                        downloadOnSavePress={true}
+                        downloadFileExtension="webm"
                         />
-                      
-              </form>
-
-              <form className="browseAudio" id="myForm" method="POST" encType="multipart/form-data" onSubmit={StartDubbing}>
-                <label htmlFor="input-file">Browse Audio</label>
-                <label style={{visibility:"hidden"}} htmlFor="inputButton"></label>
                         
-                {audioFileName?.name && (
-                  <p className="audioFileName">
-                    {audioFileName.name.split(" ").slice(0, 2).join(" ")}
-                    {audioFileName.name.split(" ").length > 2 && "..."}
-                  </p>
-                )}
-                <input
-                  type="file"
-                          id="input-file"
-                          name="source"
-                  accept="audio/mp3"
-                  style={{ display: "none" }}
-                  onChange={handleAudioChange}
-                />
-              </form>
+                      </div>
+                      
+                  </div>
+                </div>
+              </div>
 
-              
+              <div className="startButton">
+                <input value="Generate" id="inputButton" form="myForm" type="submit" />
+
+              </div>
             </div>
 
-            <div className="recordAudio">
-              
-              <div className="recordVideo ">
-                <Tooltip title="Start Recording">
-                  <button onClick={handleCameraClick}>
-                    {" "}
-                    <i className="fas fa-camera"></i>
-                  </button>
-                </Tooltip>
-                {recordedChunks.length > 0 && (
-                  <Tooltip title="Download Recording">
-                    <button onClick={handleDownload}>
-                      <i className="fas fa-download"></i>
-                    </button>
-                  </Tooltip>
+            <div className="mainCamera">
+              <div className="camera">
+                {camera ? (
+                  <Webcam
+                    height={1080}
+                    width={1920}
+                    audio={true}
+                    mirrored={true}
+                    ref={webcamRef}
+                    videoConstraints={videoConstraints}
+                  />
+                ) : (
+                  <></>
                 )}
               </div>
-              <div className="recordAudioButton">
-                <AudioRecorder
-                  onRecordingComplete={addAudioElement}
-                  audioTrackConstraints={{
-                    noiseSuppression: true,
-                    echoCancellation: true,
-                  }}
-                  downloadOnSavePress={true}
-                  downloadFileExtension="webm"
-                />
+
+              <div className="onCameraIcons">
+                {capturing ? (
+                  <Tooltip title="Stop">
+                    <button className="stop" onClick={handleStopCaptureClick}>
+                      <i className="fas fa-square"></i>
+                    </button>
+                  </Tooltip>
+                ) : (
+                  <>
+                    {camera && showButtons && (
+                      <>
+                        <Tooltip title="Start">
+                          <button className="start" onClick={handleStartCaptureClick}>
+                            <i className="fas fa-circle"></i>
+                          </button>
+                        </Tooltip>
+                        <Tooltip title="Cancel">
+                          <button className="cancel" onClick={handleCameraClick}>
+                            <i className="fas fa-times"></i>
+                          </button>
+                        </Tooltip>
+                      </>
+                    )}
+                  </>
+                )}
               </div>
             </div>
           </div>
-        </div>
-
-        <div className="startButton">
-          <input value="Generate" id="inputButton" form="myForm" type="submit" />
-          
-        </div>
-      </div>
-
-      <div className="mainCamera">
-        <div className="camera">
-          {camera ? (
-            <Webcam
-              height={1080}
-              width={1920}
-              audio={true}
-              mirrored={true}
-              ref={webcamRef}
-              videoConstraints={videoConstraints}
-            />
-          ) : (
-            <></>
-          )}
-        </div>
-
-        <div className="onCameraIcons">
-          {capturing ? (
-            <Tooltip title="Stop">
-              <button className="stop" onClick={handleStopCaptureClick}>
-                <i className="fas fa-square"></i>
-              </button>
-            </Tooltip>
-          ) : (
-            <>
-              {camera && showButtons && (
-                <>
-                  <Tooltip title="Start">
-                    <button className="start" onClick={handleStartCaptureClick}>
-                      <i className="fas fa-circle"></i>
-                    </button>
-                  </Tooltip>
-                  <Tooltip title="Cancel">
-                    <button className="cancel" onClick={handleCameraClick}>
-                      <i className="fas fa-times"></i>
-                    </button>
-                  </Tooltip>
-                </>
-              )}
-            </>
-          )}
-        </div>
-      </div>
-    </div>
+        </Fragment>
+      )}
     </Fragment>
-    )}
-  </Fragment>
-);
+  );
 };
+
 export default StartVideoRecordUpload;
